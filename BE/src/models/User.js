f5bf8ac0cbe2a2d699faf11fd80da98f3d62e0fe
Darshana Ujad <darshana.ujad@gmail.com>
2025-09-13 21:16:32 +0530

    changeg added

--- BE/src/models/User.js
+++ BE/src/models/User.js
@@ -1,8 +1,21 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
+    fullName: {type:String,required:true},
+    userName: {type:String,required:true},
+    email: {type:String,required:true},
+    password: {type:String , required:true},
+    profilePic: {type:String,default:''},
+    userCart: {type:[String],default:[]},
+    userFav: {type:[String],default:[]},
+    bio: {type:String,default:''},
+    localAddress: {type:String,default:''},
+    city: {type:String,default:''},
+    state: {type:String,default:''},
+    country: {type:String,default:''},
+    purchaseBook: {type:[String],default:[]}
 
-})
+});
 
 const User = mongoose.model('User',userSchema);
 
