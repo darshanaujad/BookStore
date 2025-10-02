e6cc949baac74d5ea43e529343f622e947de8a9a
Darshana Ujad <darshana.ujad@gmail.com>
2025-10-02 15:59:14 +0530

    Added changes

--- FE/src/App.jsx
+++ FE/src/App.jsx
@@ -1,18 +1,25 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import Home from './pages/Home'
+import Books from './pages/Books'
+import Categories from './pages/Categories'
+import AuthPage from './pages/AuthPage'
 import './App.css'
 import {Routes , Route} from 'react-router'    
 
 function App() {
    return (
     <>
-        <div className="app text-[#e0e0ca] w-full h-400 bg-red-600 text-[20px]">
+        <div>
             <Routes>
-                
+                < Route path = '/' element = {<Home/>}/>
+                < Route path = '/Books' element = {<Books/>}/>
+                 < Route path = '/Categories' element = {<Categories/>}/>
+                  < Route path = '/AuthPage' element = {<AuthPage/>}/>
             </Routes>
         </div>
     </>
-   )
+   );
 }
 export default App
