24e35915985202ed1f8eef09d40557a09b48bec0
Darshana Ujad <darshana.ujad@gmail.com>
2025-10-03 16:34:09 +0530

    Added changes

--- FE/src/App.jsx
+++ FE/src/App.jsx
@@ -6,20 +6,26 @@ import Books from './pages/Books'
 import Categories from './pages/Categories'
 import AuthPage from './pages/AuthPage'
 import './App.css'
-import {Routes , Route} from 'react-router'    
+import { Routes, Route } from 'react-router'
+import toast, { Toaster } from 'react-hot-toast'
+
 
 function App() {
-   return (
-    <>
-        <div>
-            <Routes>
-                < Route path = '/' element = {<Home/>}/>
-                < Route path = '/Books' element = {<Books/>}/>
-                 < Route path = '/Categories' element = {<Categories/>}/>
-                  < Route path = '/AuthPage' element = {<AuthPage/>}/>
-            </Routes>
-        </div>
-    </>
-   );
+    return (
+        <>
+            <div>
+                <button onClick={()=>{toast.success("This is toast success.")}}>Success</button> <br/>
+                <button>Error</button><br/>
+                <button>Loading</button><br/>
+                <Routes>
+                    < Route path='/' element={<Home />} />
+                    < Route path='/Books' element={<Books />} />
+                    < Route path='/Categories' element={<Categories />} />
+                    < Route path='/AuthPage' element={<AuthPage />} />
+                </Routes>
+                <Toaster position='top-right' reverseOrder={false} />
+            </div>
+        </>
+    );
 }
 export default App
