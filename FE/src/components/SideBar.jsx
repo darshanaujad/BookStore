caf1a6d77c33e438348d226904c82b944a932006
Darshana Ujad <darshana.ujad@gmail.com>
2025-10-04 17:45:06 +0530

    added changes

--- FE/src/components/SideBar.jsx
+++ FE/src/components/SideBar.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
+import { Search, ShoppingCartIcon } from 'lucide-react'
 
 const SideBar = () => {
   return (
-    <div>
-      
-    </div>
+    <header>
+      <nav className='w-screen p-4 border-0 border-b-black flex justify-between px-20 items-center'>
+        <div className=''>
+            Logo
+        </div>
+        <ul className='flex gap-6 text-black/70'>
+            <li className='transition-colors duration-200 cursor-pointer ease-in-out hover:text-blue-400'>Home</li>
+            <li className='transition-colors duration-200 cursor-pointer ease-in-out hover:text-blue-400'>Books</li>
+            <li className='transition-colors duration-200 cursor-pointer ease-in-out hover:text-blue-400'>Categories</li>
+            <li className='transition-colors duration-200 cursor-pointer ease-in-out hover:text-blue-400'>Author</li>
+            <li className='transition-colors duration-200 cursor-pointer ease-in-out hover:text-blue-400'>Contact</li>
+        </ul>
+        <div className='flex gap-3'>
+            <span><Search size={18}/></span>
+            <span><ShoppingCartIcon size={18}/></span>
+        </div>
+      </nav>
+    </header>
   )
 }
 
