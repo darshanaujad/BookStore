fdfa03fc33b7db821472f5234663714d82ab5e42
Darshana Ujad <darshana.ujad@gmail.com>
2025-10-05 14:18:03 +0530

    Added changes

--- FE/src/components/SideBar.jsx
+++ FE/src/components/SideBar.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Search, ShoppingCartIcon } from 'lucide-react'
+import { Heart, Search, ShoppingCartIcon } from 'lucide-react'
+import {useNavigate} from 'react-router'
 
 const SideBar = () => {
   const menuItems = [
@@ -9,6 +10,7 @@ const SideBar = () => {
     { name: 'Author', link: '/author' },
     { name: 'Contact', link: '/contact' }
   ];
+  const navigate = useNavigate();
   return (
     <header>
       <nav className='w-screen p-4 border-0 border-b-black flex justify-between px-20 items-center'>
@@ -16,11 +18,12 @@ const SideBar = () => {
           <img className = 'w-35 object-center' src="/BookNestLogo.png" alt="BookNestLogo" />
         </div>
         <ul className='flex gap-6 text-black/70'>
-          {menuItems.map((items) => (<li key={items.name} className='transition-colors duration-200 cursor-pointer ease-in-out hover:text-blue-400'>{items.name}</li>))}
+          {menuItems.map((items) => (<li key={items.name} className='transition-colors duration-200 cursor-pointer ease-in-out hover:text-blue-400' onClick={()=>navigate(items.link)}>{items.name}</li>))}
         </ul>
-        <div className='flex gap-3'>
-          <span><Search size={18} /></span>
-          <span><ShoppingCartIcon size={18} /></span>
+        <div className='flex gap-4 '>
+          <span className='text-black/70 transition-colors duration-200 cursor-pointer ease-in-out hover:text-blue-400 '><Search size={18} /></span>
+          <span className='text-black/70 transition-colors duration-200 cursor-pointer ease-in-out hover:text-blue-400'><Heart size = {18}/></span>
+          <span className='text-black/70 transition-colors duration-200 cursor-pointer ease-in-out hover:text-blue-400'><ShoppingCartIcon size={18} /></span>
         </div>
       </nav>
     </header>
