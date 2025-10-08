d5b0b67282937bac3fba3bbe80e86da70da08a1d
Darshana Ujad <darshana.ujad@gmail.com>
2025-10-08 18:15:17 +0530

    addded changes

--- FE/src/pages/AuthPage.jsx
+++ FE/src/pages/AuthPage.jsx
@@ -4,11 +4,11 @@ const AuthPage = () => {
   return (
     <div className='h-screen flex justify-center items-center'>
       {/*leftside*/}
-      <div className='w-[43%] bg-blue-400 h-full'>
-
+      <div className='w-[43%] bg-[#fefefe] h-full'>
+            
       </div>
       {/* Right side*/}
-      <div className='w-[57%] bg-amber-200 h-full'>
+      <div className='w-[57%] bg-[#60b8e8] h-full'>
 
       </div>
     </div>
