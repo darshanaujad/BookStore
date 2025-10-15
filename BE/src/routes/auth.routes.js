3a9a39e55e6ab0e9cad1b97bde26659be74a5fca
Darshana Ujad <darshana.ujad@gmail.com>
2025-10-15 16:38:51 +0530

    added changes

--- BE/src/routes/auth.routes.js
+++ BE/src/routes/auth.routes.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const authMiddlewre = require('../middlewares/authMiddleware');
 const router = express.Router();
-const {loginUser , registerUser} = require('../controllers/auth.controllers');
+const {loginUser , registerUser , getMe} = require('../controllers/auth.controllers');
 
 router.post('/login',loginUser);
 router.post('/signup',registerUser);
+router.get('/me' , authMiddlewre , getMe)
 
 module.exports = router;
+
