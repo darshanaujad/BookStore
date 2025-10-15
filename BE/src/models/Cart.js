650b6a8e21e23d7beb799946f948fb6c9e0339fa
Darshana Ujad <darshana.ujad@gmail.com>
2025-10-15 16:11:33 +0530

    added changes

--- BE/src/models/Cart.js
+++ BE/src/models/Cart.js
@@ -1,9 +1,25 @@
-const mongoose = require('monngoose');
+const mongoose = require('mongoose');
 
 const cartSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId, ref: 'User',
+        required: true
+    },
+    products: {
+        type: [{
+            productId: {
+                type: mongoose.Schema.Types.ObjectId, ref: 'Book',
+                required: true
+            },
+            qauntity: {
+                type: Number ,
+                required: true 
+            }
+        }]
 
+    }
 });
 
-const cart = mongoose.model("Cart" , cartSchema);
+const cart = mongoose.model("Cart", cartSchema);
 
-module.exports = Cart ;
+module.exports = Cart;
