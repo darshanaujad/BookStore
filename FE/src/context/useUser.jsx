f3345c29580805b5ac127525ac2ffbd2a32924c3
Darshana Ujad <darshana.ujad@gmail.com>
2025-10-28 08:13:41 +0530

    Added changes

--- FE/src/context/useUser.jsx
+++ FE/src/context/useUser.jsx
@@ -1,2 +1,20 @@
 import axios from 'axios'
-import {useState, useEffect , createContext , useContext} from 'react'
+import { useState, useEffect, createContext, useContext } from 'react'
+const API_URL = import.meta.env.VITE_API_URL
+
+const UserContext = createContext()
+
+const UserProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+    useEffect(() => {
+        const fetchUser = async ()=>{
+            try {
+                const res 
+            } catch (error) {
+                
+            }
+        }
+    }, [])
+}
+
+
