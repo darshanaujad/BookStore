cef0950dc1f1f912f86c3ffa9dfebfd2b6913c46
Sarthak Joshi <sarthakjoshi12005@gmail.com>
2025-11-03 21:08:28 +0530

    Update book.routes.js

--- BE/src/routes/book.routes.js
+++ BE/src/routes/book.routes.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
-
-const {addBook , getBook , getBookById , deleteBookById} = require('../controllers/book.controllers');
+const { addBook, getBookById, getAllBooks, deleteBookById } = require('../controllers/book.controller');
 
 router.post('/book', addBook);
-router.get('/book' , getBook);
-router.get('/book/:id' , getBookById);
-router.delete('/book/:id' , deleteBookById);
+router.get('/books', getAllBooks);
+router.get('/book/:id', getBookById);
+router.delete('/book/:id', deleteBookById);
+
 
 module.exports = router;
