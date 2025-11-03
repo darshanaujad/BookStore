62e72b3e60b5ccc0af32d882edeb1db480be3257
Sarthak Joshi <sarthakjoshi12005@gmail.com>
2025-11-03 21:22:45 +0530

    Set API_URL from environment variable

--- FE/src/library/axiosInstance.js
+++ FE/src/library/axiosInstance.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-
+const API_URL = import.meta.env.VITE_API_URL;
 const axiosInstance = axios.create({
     baseURL: API_URL || "http://localhost:5000/api",
     headers: {
