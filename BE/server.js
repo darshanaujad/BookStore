510ff8a9793f76bc253c36fa68904ef02090b91f
Sarthak Joshi <sarthakjoshi12005@gmail.com>
2025-11-03 21:08:51 +0530

    Update server.js

--- BE/server.js
+++ BE/server.js
@@ -1,44 +1,53 @@
 require('dotenv').config();
-const rateLimit = require('express-rate-limit');
 const express = require('express');
+const rateLimit = require('express-rate-limit');
 const cors = require('cors');
-const app = express();
+
 require('./src/config/db');
 
+const app = express();
 const PORT = process.env.PORT;
+const client_url1 = process.env.CLIENT_URL1
+const client_url2 = process.env.CLIENT_URL2
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
+
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:5174'],
+    origin: [client_url1, client_url2],
     credentials: true
 }));
 
-// Create a rate limiter
 const limiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100, // limit each IP to 100 requests per window
-    message: "Too many requests, please try again later.",
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per windowMs
+  message: {
+    success: false,
+    message: "Too many requests from this IP, please try again after 15 minutes."
+  },
+  standardHeaders: true, // Return rate limit info in headers
+  legacyHeaders: false, // Disable X-RateLimit headers
 });
 
 // Apply limiter to all routes
 app.use(limiter);
 
-app.get('/', (req, res) => {
-    res.send('Server is running...');
-})
 
 const authRoutes = require('./src/routes/auth.routes');
-app.use('/api/auth', authRoutes);
-
 const categoryRoutes = require('./src/routes/category.routes');
-app.use('/api/category', categoryRoutes);
-
 const bookRoutes = require('./src/routes/book.routes');
+
+
+app.get('/', (req, res) => {
+    res.send("Server is running..");
+});
+
+
+app.use('/api/auth', authRoutes);
+app.use('/api/category', categoryRoutes);
 app.use('/api/book', bookRoutes);
 
 
 app.listen(PORT, () => {
     console.log(`server is running on ${PORT}`);
-})
-
+});
