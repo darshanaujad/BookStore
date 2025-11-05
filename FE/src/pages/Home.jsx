79154cafc011e898e4c04acd82ba70135d88ca29
Darshana Ujad <darshana.ujad@gmail.com>
2025-11-05 22:32:18 +0530

    added changes

--- FE/src/pages/Home.jsx
+++ FE/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react"
 import { useUser } from "../context/useUser";
 import { useNavigate } from 'react-router';
 import { fetchBooks } from "../library/api";
-import { RefreshCcwDotIcon, TractorIcon, Truck, TruckElectric, TruckIcon } from 'lucide-react';
+import { FileMusic, KeyRoundIcon, MoveDiagonal, Music, Pyramid, RefreshCcwDotIcon, SuperscriptIcon, TractorIcon, Truck, TruckElectric, TruckIcon, UserRoundCheckIcon, UserRoundPlusIcon } from 'lucide-react';
 import FloatingGrid from "../components/FloatingGrid"; 
 
 
@@ -28,7 +28,7 @@ const Home = () => {
 
   return (
     <>
-      <section className="h-screen ">
+      <section className="h-screen">
         <div className="h-[80%] flex gap-2 justify-center bg-gradient-to-br from-[#fefefe]  to-[#ffeaea]">
           <div className="w-[600px]  flex justify-center flex-col px-20 gap-3">
             <div>
@@ -50,8 +50,30 @@ const Home = () => {
             <FloatingGrid />
           </div>
         </div>
-        <div className="h-[20%] bg-[#d1e7ea]">
-
+        <div className="h-[20%] bg-[#d1e7ea] flex gap-5 items-center justify-center">
+              <div className="w-[18%] h-[90%] bg-white rounded-2xl grid place-items-center" >
+                <p> <span className="text-green-500 text-center"><TruckIcon size={30} /></span></p>
+                <h2 className="font-bold text-2xl">Free Shipping</h2>
+                <p className="font-semibold">On orders over ₹900</p>
+              </div>
+              <div className="w-[18%] h-[90%] bg-white rounded-2xl grid place-items-center" >
+                <p> <span className="text-green-500 text-center"><RefreshCcwDotIcon size={30} /></span></p>
+                <h2 className="font-bold text-2xl">Easy Returns</h2>
+                <p className="font-semibold">7-day hassle-free returns</p>
+              </div>
+              <div className="w-[18%] h-[90%] bg-white rounded-2xl grid place-items-center" >
+                <p> <span className="text-green-500 text-center"><Pyramid size={30} /></span></p>
+                <h2 className="font-bold text-2xl">Secure Payments</h2>
+                <p className="font-semibold">SSL encrypted transactions</p>
+              </div>
+              <div className="w-[18%] h-[90%] bg-white rounded-2xl grid place-items-center" >
+                <p> <span className="text-green-500 text-center"><UserRoundCheckIcon size={30} /></span></p>
+                <h2 className="font-bold text-2xl">24/7 Support</h2>
+                <p className="font-semibold">Round-the-clock assistant</p>
+              </div>
+             
+              
+              
         </div>
       </section>
     </>
